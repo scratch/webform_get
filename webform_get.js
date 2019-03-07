4393d4104739ce1ef1d27cdb0a36570af8e083d8
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-03-07 23:54:19 +0530

    rewrite using postman

--- webform_get.js
+++ webform_get.js
@@ -11,66 +11,47 @@
  */
 
 var request = require('request')
-
-
-var purl = 'http://192.168.1.45:32798/user/login'
-// var webfrm_url = 'http:/192.168.1.45:32798/webform_rest/swl_create_service/submission/1?_format=json'
-var webfrm_url = 'http://192.168.1.45:32798/webform_rest/swl_create_service/submission/1'
 // The platform.sh stuff.
 // var purl = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/user/login?'
 // var webfrm_url = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/webform_rest/swl_create_service/submissions/1_format=json?'
-
-function req_callback_webform (err, resp, body) {
-    if (err)  {
-        console.error ("Error authenticating", err);
-    } else {
-        console.log ("Response: ")
-        console.log ("Body: ")
-    }
-}
-
-
-var options_webfrm = {
-    url: webfrm_url,
-    jar: true,
-    json: true,
-    auth: {
-        'bearer': 'bearerToken'
-    }
-};
-
-
-function req_callback (err, resp, body) {
-    if (err)  {
-        console.error ("Error authenticating", err);
-    } else {
-        console.log ("Response: ")
-        console.log ("Body: ")
-        request.get(options_webfrm, req_callback_webform).on ('data', function(data) {
-            console.log ('data: ' + data)
-        });
-    }
-}
 
 
 //
 // main()
 //
 function getWebformSubmission()  {
-    auth = 'Basic' + new Buffer("admin:swladmin").toString('base64');
-
+    // auth = 'Basic' + new Buffer("admin:swladmin").toString('base64');
+    var purl = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/user/login?_format=json'
     var options = {
         url: purl,
+        json: true,
         jar: true,
-        headers: {
-            "Authorization": auth
+        body: {
+          name: 'admin',
+          pass: 'swladmin'
         }
     }
 
-    console.log ("\n\n\nJUST A BIG LINE --- \n\n\n");
-    request.get(options, req_callback)
-    console.log ("\n\n\nYET ANOTHER  BIG LINE --- \n\n\n");
+    request.post(options, (err, resp, body) => {
+        if (err)  {
+            console.error ("Error authenticating", err);
+        } else {
+          console.log('Login output ', JSON.stringify(resp.body, null, 2));
+          var webfrm_url = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/webform_rest/swl_create_service/submission/1?_format=json';
+            var options_webfrm = {
+                url: webfrm_url,
+                headers: [{
+                  'X-CSRF-Token': resp.body.csrf_token,
+                  'cache-control': 'no-cache'
+                }],
+                json: true,
+                jar: true
+            };
+            request.get(options_webfrm, (err, resp, body) => {
+                console.log('Form data ', JSON.stringify(resp.body, null, 2));
+            });
+        }
+    });
 }
 
 getWebformSubmission();
-
