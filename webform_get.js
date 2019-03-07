7850e62a92427940a350ff5431f2be25072da192
Krishnan N <krishnan.n@gai.co.in>
2019-03-07 08:11:11 +0530

    Just added TODO's

--- webform_get.js
+++ webform_get.js
@@ -1,5 +1,13 @@
 /* get webform submissions from Drupal 8 site. SHMG.
  *
+ * TODO:
+ * 1. Make the request() calls work sequentially, without calling the second
+ * request() inside the callback
+ * 2. Get cookie result of login request.
+ * 3. Make login REST call work with ?_format=json
+ * 4. Make login REST call work with POST (same as #3?)
+ * 5. nodejs resource; which website?
+ * 6. Finally, make webform GET to work!
  */
 
 var request = require('request')
@@ -8,6 +16,7 @@ var request = require('request')
 var purl = 'http://192.168.1.45:32798/user/login'
 // var webfrm_url = 'http:/192.168.1.45:32798/webform_rest/swl_create_service/submission/1?_format=json'
 var webfrm_url = 'http://192.168.1.45:32798/webform_rest/swl_create_service/submission/1'
+// The platform.sh stuff.
 // var purl = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/user/login?'
 // var webfrm_url = 'https://master-7rqtwti-bzrfe2fjw7z3k.eu-2.platformsh.site/webform_rest/swl_create_service/submissions/1_format=json?'
 
@@ -29,6 +38,7 @@ var options_webfrm = {
         'bearer': 'bearerToken'
     }
 };
+
 
 function req_callback (err, resp, body) {
     if (err)  {
